931fd97a63b9e8399d78a8d8aea094358ec72172
Teja <tejanagesh12345@gmail.com>
2021-03-28 16:55:38 +0530

    Added UserService

---
+++ T-WashUserService/src/main/java/com/twash/userservice/TWashUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.twash.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWashUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWashUserServiceApplication.class, args);
+	}
+
+}
