af31c650fc7c55acbb4b3f5745e7cbccc1ae9d8e
Teja <tejanagesh12345@gmail.com>
2021-04-02 13:20:53 +0530

    Implemented first version of Userservice

---
+++ T-WashUserService/src/main/java/com/twash/userservice/repository/UsersRepository.java
@@ -0,0 +1,20 @@
+package com.twash.userservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+import com.twash.userservice.model.Users;
+
+/**
+ * UsersRepository Interface Extends MongoRepository Interface For CRUD Operations 
+ *
+ */
+public interface UsersRepository extends MongoRepository<Users, Long>{
+	
+	Optional<Users> findByEmail(String email);
+	Optional<List<Users>> findByRole(String role);
+	
+}
